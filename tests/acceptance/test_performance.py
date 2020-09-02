0f8b9d971167774d01887f7423108bf4f84f7740
Olav Stetter <olav.stetter@googlemail.com>
2020-09-02 20:53:38 +0200

    Added minimal performance test.

---
+++ tests/acceptance/test_performance.py
@@ -0,0 +1,15 @@
+import numpy as np  # type: ignore
+import time
+
+from uniplot import plot
+
+
+def test_plotting_one_million_points():
+    nr_samples = 1_000_000
+    acceptable_time_in_seconds = 0.2
+
+    ys = np.random.normal(0, 1, nr_samples)
+
+    start_time = time.time()
+    plot(ys)
+    assert time.time() - start_time < acceptable_time_in_seconds
