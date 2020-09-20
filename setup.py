e7f63269f6bf38d052b9aa8bd5d47c4ad7c2098b
Olav Stetter <olav.stetter@googlemail.com>
2020-09-20 20:00:40 +0200

    Included demo GIF also in PyPI Readme. Version bump.

--- setup.py
+++ setup.py
@@ -2,20 +2,16 @@ from setuptools import setup  # type: ignore
 
 from version import version
 
-long_description = """Simple plotting tool.
+long_description = """Lightweight plotting to the terminal. 4x resolution via Unicode.
+
+[![uniplot demo GIF](https://github.com/olavolav/uniplot/raw/master/resource/uniplot-demo.gif)](https://asciinema.org/a/Ldgn5pHOgxPJmIf2ZvlfIPR3L)
 
 When working with production data science code it can be handy to have plotting
 tool that does not rely on graphics dependencies or works only in a Jupyter notebook.
 
 The **use case** that this was built for is to have plots as part of your data science /
 machine learning CI pipeline - that way whenever something goes wrong, you get not only
-the error and backtrace but also plots that show what the problem was.
-
-Demo:
-[On asciinema](https://asciinema.org/a/Ldgn5pHOgxPJmIf2ZvlfIPR3L)
-[On Youtube](https://youtu.be/rZxGAGMjP5k)
-
-[![asciicast](https://asciinema.org/a/Ldgn5pHOgxPJmIf2ZvlfIPR3L.png)](https://asciinema.org/a/Ldgn5pHOgxPJmIf2ZvlfIPR3L)"""
+the error and backtrace but also plots that show what the problem was."""
 
 
 setup(
