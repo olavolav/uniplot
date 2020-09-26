3acae952a0c86a59351cfcb7c387aaa33053a5d2
Olav Stetter <olav.stetter@googlemail.com>
2020-09-26 10:48:49 +0200

    Inverse discretizer now also works on lists.

--- tests/unit/test_discretizer.py
+++ tests/unit/test_discretizer.py
@@ -1,6 +1,15 @@
 import numpy as np  # type: ignore
 
-from uniplot.discretizer import discretize, compute_y_at_middle_of_row
+from uniplot.discretizer import (
+    discretize,
+    compute_y_at_middle_of_row,
+    invert_discretize,
+)
+
+
+####################
+# Test: discretize #
+####################
 
 
 def test_correct_discretization_for_number():
@@ -12,6 +21,11 @@ def test_correct_discretization_for_array():
     vector_float = np.array([0.01, 0.99, 1.01, 1.5, 1.99, 9.99])
     vector_integer = discretize(x=vector_float, x_min=0, x_max=10, steps=10)
     assert (vector_integer == np.array([0, 0, 1, 1, 1, 9])).all()
+
+
+####################################
+# Test: compute_y_at_middle_of_row #
+####################################
 
 
 def test_compute_y_at_middle_of_row_simple_case():
@@ -28,3 +42,23 @@ def test_compute_y_at_middle_of_row_complex_case():
     )
     y_level_as_designed = 9.25
     assert abs(y_level - y_level_as_designed) < 0.01
+
+
+###########################
+# Test: invert_discretize #
+###########################
+
+
+def test_invert_discretize_with_single_index():
+    level = invert_discretize(2, minimum=0, maximum=1, nr_bins=3)
+    level_as_designed = 5 / 6
+    assert abs(level - level_as_designed) < 0.01
+
+
+def test_invert_discretize_with_list_of_indices():
+    levels = invert_discretize([0, 9, 1], minimum=0, maximum=10, nr_bins=10)
+    levels_as_designed = [1 / 2, 9 + 1 / 2, 3 / 2]
+
+    assert abs(levels[0] - levels_as_designed[0]) < 0.01
+    assert abs(levels[1] - levels_as_designed[1]) < 0.01
+    assert abs(levels[2] - levels_as_designed[2]) < 0.01
--- uniplot/discretizer.py
+++ uniplot/discretizer.py
@@ -23,5 +23,23 @@ def compute_y_at_middle_of_row(
 
     Typical use case is to display the right axis tick.
     """
-    step_size = (y_max - y_min) / height
-    return (height - (height_index_from_top + 0.5)) * step_size + y_min
+    return invert_discretize(
+        i=height - height_index_from_top - 1,
+        minimum=y_min,
+        maximum=y_max,
+        nr_bins=height,
+    )
+
+
+def invert_discretize(
+    i: Union[int, np.array], minimum: float, maximum: float, nr_bins: int
+) -> Union[int, np.array]:
+    """
+    Returns the level at the middle of the specified bin.
+
+    This is the inverse of `discretizer.discretize`.
+    """
+    assert maximum > minimum
+
+    step_size = (maximum - minimum) / nr_bins
+    return ((np.asarray(i) + 0.5) * step_size + minimum).astype(float)
