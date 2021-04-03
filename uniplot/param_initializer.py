2d46e5b2176a2435081caa7958eb86169a3c65d9
Olav Stetter <olav.stetter@googlemail.com>
2021-04-03 09:24:01 +0200

    Cleanup: Bounding box initialization.

--- uniplot/param_initializer.py
+++ uniplot/param_initializer.py
@@ -4,25 +4,29 @@ from typing import Dict, Tuple, List, Optional, Any
 from uniplot.multi_series import MultiSeries
 from uniplot.options import Options
 
+AUTO_WINDOW_ENLARGE_FACTOR = 1e-3
+
 
 def validate_and_transform_options(series: MultiSeries, kwargs: Dict = {}) -> Options:
     """
     This will check the keyword arguments passed to the `uniplot.plot` function, will transform them and will return them in form of an `Options` object.
     """
-    # Set bounds to show all points by default
-    kwargs["x_min"] = kwargs.get("x_min", series.x_min())
-    kwargs["x_max"] = kwargs.get(
-        "x_max", series.x_max() + 1e-4 * (series.x_max() - series.x_min())
-    )
+    # Set x bounds to show all points by default
+    x_enlarge_delta = AUTO_WINDOW_ENLARGE_FACTOR * (series.x_max() - series.x_min())
+    kwargs["x_min"] = kwargs.get("x_min", series.x_min() - x_enlarge_delta)
+    kwargs["x_max"] = kwargs.get("x_max", series.x_max() + x_enlarge_delta)
 
+    # Fallback for only a single data point, or multiple with single x coordinate
     if float(kwargs["x_min"]) == float(kwargs["x_max"]):
         kwargs["x_min"] = kwargs["x_min"] - 1
         kwargs["x_max"] = kwargs["x_max"] + 1
-    kwargs["y_min"] = kwargs.get("y_min", series.y_min())
-    kwargs["y_max"] = kwargs.get(
-        "y_max", series.y_max() + 1e-4 * (series.y_max() - series.y_min())
-    )
 
+    # Set y bounds to show all points by default
+    y_enlarge_delta = AUTO_WINDOW_ENLARGE_FACTOR * (series.y_max() - series.y_min())
+    kwargs["y_min"] = kwargs.get("y_min", series.y_min() - y_enlarge_delta)
+    kwargs["y_max"] = kwargs.get("y_max", series.y_max() + y_enlarge_delta)
+
+    # Fallback for only a single data point, or multiple with single y coordinate
     if float(kwargs["y_min"]) == float(kwargs["y_max"]):
         kwargs["y_min"] = kwargs["y_min"] - 1
         kwargs["y_max"] = kwargs["y_max"] + 1
