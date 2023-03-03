2a559f6e149895a6f7a4f30f69b29d54c1eea995
Wouter Meijer <40892865+h0uter@users.noreply.github.com>
2023-03-03 23:29:44 +0100

    added __all__ statement to fix language server complaint when using `from uniplot import plot`

--- uniplot/__init__.py
+++ uniplot/__init__.py
@@ -1,3 +1,5 @@
 # Add a shortcut such that users of the library can write `from uniplot import plot`
 # instead of `from uniplot.uniplot import plot`.
 from uniplot.uniplot import plot, plot_to_string, histogram
+
+__all__ = ["plot", "plot_to_string", "histogram"]
