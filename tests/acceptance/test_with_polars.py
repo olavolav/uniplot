e2eb2f3ba884e63aa5bf2a300cc33cd6e680a4b0
Olav Stetter <olav.stetter@googlemail.com>
2024-01-23 16:40:12 +0100

    Added tests to confirm Polars support.

--- tests/acceptance/test_with_polars.py
+++ tests/acceptance/test_with_polars.py
@@ -1,16 +1,16 @@
 """
-Plotting with pandas is such a common use case in data science that we want to
+Plotting with polars is such a common use case in data science that we want to
 make sure it works nicely with uniplot. Thus these tests.
 """
 
-import pandas as pd  # type: ignore
+import polars as pl
 import numpy as np  # type: ignore
 
 from uniplot import plot
 
 
 def test_normal_plotting():
-    data = pd.DataFrame(
+    data = pl.DataFrame(
         data={
             "asset": ["asset1", "asset1", "asset2", "asset3"],
             "speed": [20.0, 50.0, 75.6, 12.6],
@@ -21,7 +21,7 @@ def test_normal_plotting():
 
 
 def test_logarithmic_plotting():
-    data = pd.DataFrame(
+    data = pl.DataFrame(
         data={
             "asset": ["asset1", "asset1", "asset2", "asset3"],
             "speed": [20.0, 50.0, 75.6, 12.6],
@@ -31,10 +31,10 @@ def test_logarithmic_plotting():
     plot(xs=data["speed"], ys=data["vertical_rms"])
 
 
-def test_that_pandas_series_is_interpreted_as_single_dim_array():
+def test_that_polars_series_is_interpreted_as_single_dim_array():
     from uniplot.multi_series import MultiSeries
 
-    data = pd.DataFrame(
+    data = pl.DataFrame(
         data={
             "asset": ["asset1", "asset1", "asset2", "asset3"],
             "speed": [20.0, 50.0, 75.6, 12.6],
@@ -47,14 +47,15 @@ def test_that_pandas_series_is_interpreted_as_single_dim_array():
 
 
 def test_grouped_plotting():
-    data = pd.DataFrame(
+    data = pl.DataFrame(
         data={
             "asset": ["asset1", "asset1", "asset2", "asset3"],
             "speed": [20.0, 50.0, 75.6, 12.6],
             "vertical_rms": [12.4, 23.5, 55.3, 20.5],
         }
     )
-    grouped_data = data.groupby("asset")
+    # TODO Should find a better way than with the `maintain_order` option
+    grouped_data = data.group_by(["asset"], maintain_order=True)
     plot(
         xs=[group["speed"] for (_, group) in grouped_data],
         ys=[group["vertical_rms"] for (_, group) in grouped_data],
