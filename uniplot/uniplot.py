64c9fcb3c91554e7bbe8e5d62309886288070a46
Olav Stetter <olav.stetter@googlemail.com>
2024-03-21 18:12:25 +0100

    Histogram bin limits improvement.

--- uniplot/uniplot.py
+++ uniplot/uniplot.py
@@ -134,16 +134,23 @@ def histogram(
     # Histograms usually make sense only with lines
     kwargs["lines"] = kwargs.get("lines", True)
 
-    auto_bin_limits: bool = bins_min is None and bins_max is None
+    bins_min_real: float = bins_min or multi_series.y_min()
+    bins_max_real: float = bins_max or multi_series.y_max()
+    assert bins_max_real > bins_min_real
 
-    bins_min = bins_min or multi_series.y_min()
-    bins_max = bins_max or multi_series.y_max()
-    assert bins_max > bins_min
-    if auto_bin_limits:
-        delta = bins_max - bins_min
-        bins_min = bins_min - 0.1 * delta
-        bins_max = bins_max + 0.1 * delta
-    bin_edges = [bins_min + i * (bins_max - bins_min) / bins for i in range(bins + 1)]
+    # Depending on whether the bin limits were supplied as arguments, expand
+    # the width automatically
+    delta: float = bins_max_real - bins_min_real
+    if bins_min is None:
+        bins_min_real = bins_min_real - 0.1 * delta
+    if bins_max is None:
+        bins_max_real = bins_max_real + 0.1 * delta
+
+    # Compute bin edges
+    bin_edges = [
+        bins_min_real + i * (bins_max_real - bins_min_real) / bins
+        for i in range(bins + 1)
+    ]
 
     xs_histo_series = []
     ys_histo_series = []
