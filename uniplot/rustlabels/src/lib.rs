f85277caf493facf19e9dfda967e0bc8742121f7
Olav Stetter <olav.stetter@googlemail.com>
2024-10-27 16:59:51 +0100

    First working integration with the Rust library.

--- uniplot/rustlabels/src/lib.rs
+++ uniplot/rustlabels/src/lib.rs
@@ -2,13 +2,14 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn float_axis_labels(x_min: f64, x_max: f64, available_space: u32, vertical_direction: bool, unit: String) -> PyResult<String> {
+    let labels = axis_labels_rs::float_axis_labels(x_min, x_max, available_space, 1, vertical_direction, &unit);
+    return Ok(labels);
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rustlabels(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(float_axis_labels, m)?)?;
     Ok(())
 }
