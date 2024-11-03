a7e14269ae68b840a151931ca3f9feed7be7e194
Olav Stetter <olav.stetter@googlemail.com>
2024-11-03 08:51:27 +0100

    First Braille utility function.

--- tests/unit/test_plot_elements.py
+++ tests/unit/test_plot_elements.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from uniplot.plot_elements import character_for_2by2_pixels
+from uniplot.plot_elements import character_for_2by2_pixels, character_for_2by4_pixels
 
 
 ######################################
@@ -26,3 +26,28 @@ def test_top_left_quarter_square():
 def test_left_half_square():
     square = np.array([[1, 0], [1, 0]])
     assert character_for_2by2_pixels(square) == "▌"
+
+
+######################################
+# Testing: character_for_2by4_pixels #
+######################################
+
+
+def test_braille_empty_square():
+    square = np.zeros([4, 2])
+    assert character_for_2by4_pixels(square) == "⠀"
+
+
+def test_braille_full_square():
+    square = np.ones([4, 2])
+    assert character_for_2by4_pixels(square) == "⣿"
+
+
+def test_braille_top_left_triagle():
+    square = np.array([[1, 1], [1, 0], [0, 0], [0, 0]])
+    assert character_for_2by4_pixels(square) == "⠋"
+
+
+def test_braille_bottom_right_triagle():
+    square = np.array([[0, 0], [0, 0], [0, 1], [1, 1]])
+    assert character_for_2by4_pixels(square) == "⣠"
