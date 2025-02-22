6986caa762759f07fa5aa5b05c05cee6ea61cfac
Olav Stetter <olav.stetter@googlemail.com>
2025-02-22 12:46:57 +0100

    Improved speed scripts.

--- examples/8-speed_update.py
+++ examples/8-speed_update.py
@@ -4,17 +4,8 @@ import time
 import math
 
 
-kwargs = {
-    "width": 190,
-    "height": 16,
-    "y_unit": "$",
-    "color": ["green", "red", "blue"],
-    "title": "Price",
-}
-
-
 def wave_generator(a1=1, frequency=1, phase_shift=0.4, f2=6.1343):
-    t = 0  # Start at t=0
+    t = 0
     while t < 100:  # Infinite generator
         value = (
             a1 * math.sin(frequency * t + phase_shift)
@@ -28,13 +19,14 @@ def wave_generator(a1=1, frequency=1, phase_shift=0.4, f2=6.1343):
 max_len = 200
 
 deq = deque([0] * max_len, maxlen=max_len)
-plt = plot_gen(**kwargs)
+
+plt = plot_gen(width=100, y_unit="$", title="Price", color=["green"], y_min=-5, y_max=5)
 
 start = time.perf_counter()
 for i in wave_generator():
     deq.append(i)
     deq2 = [0.8 + i * 0.3 for i in deq]
-    # plt.update(ys=[deq,deq2], title="test") # current error in title
-    plt.update(ys=[deq], title="test")  # current error in title
+    plt.update(ys=[deq])
 stop = time.perf_counter()
+
 print(f"Time: {stop - start}s")
