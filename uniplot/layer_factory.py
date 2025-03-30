b191a60b8d7407f0e75288b4b97fe0cc65aca64c
Olav Stetter <olav.stetter@googlemail.com>
2025-03-30 08:25:12 +0200

    New version works for box, no color.

--- uniplot/layer_factory.py
+++ uniplot/layer_factory.py
@@ -102,70 +102,75 @@ def render_points(xs: List[NDArray], ys: List[NDArray], options: Options) -> NDA
         )
 
     # Render the dot matrix into characters
-    # counts_matrix = np.zeros((height, width), dtype=int)
-
+    char_matrix = _init_character_matrix(width=options.width, height=options.height)
     submatrices = convert_matrix_to_rows_of_submatrices(px_matrix, width_submatrix=scaling_factor_width, height_submatrix=scaling_factor_height)
     if options.color:
-        color = submatrices.max(axis=(2)) - 1  # check color
-        mat = np.clip(submatrices, a_min=0, a_max=1)  # type: ignore
+        # color = submatrices.max(axis=(2)) - 1  # check color
+        submatrices = np.clip(submatrices, a_min=0, a_max=1)  # type: ignore
     # Encode
-    new_pix = (mat * encoder).sum(axis=(2))
+    new_pix = (submatrices * encoder).sum(axis=(2))
+    non_zero_mask = new_pix != 0
 
-    char_matrix = _init_character_matrix(width=options.width, height=options.height)
-    if options.force_ascii:
-        # If using ASCII characters
-        for row in range(options.height):
-            for col in range(options.width):
-                char_matrix[row, col] = elements.character_for_ascii_pixel(
-                    px_matrix[row, col],
-                    options.force_ascii_characters,
-                    color_mode=options.color,
-                )
-    elif options.character_set == "braille":
-        # If using Braille characters
-        for row in range(options.height):
-            for col in range(options.width):
-                char_matrix[row, col] = elements.character_for_2by4_pixels(
-                    px_matrix[4 * row : 4 * row + 4, 2 * col : 2 * col + 2],
-                    color_mode=options.color,
-                )
-    else:
-        # If using Box characters
-        color = None
-        # Used to be character_for_2by2_pixels
-        # TODO Use such a logic also for the other character sets
-        encoder = np.array([1, 2, 4, 8], ndmin=3)
-        mat = np.swapaxes(
-            px_matrix.reshape(height // 2, 2, width // 2, 2), 1, 2
-        ).reshape((height // 2, width // 2, 4))
-        if options.color:
-            color = mat.max(axis=(2)) - 1  # check color
-            mat = np.clip(mat, a_min=0, a_max=1)  # type: ignore
-        new_pix = (mat * encoder).sum(axis=(2))  # decoder
-        non_zero_mask = new_pix != 0
+    decoder_c = np.array(elements.UNICODE_SQUARES)
+    index = new_pix[non_zero_mask]
 
-        if options.color:
-            assert color is not None
-            colors = (
-                [COLOR_CODES[c] for c in options.color]
-                if isinstance(options.color, list)
-                else COLOR_CODES.values()
-            )
-            decoder_c = np.array(
-                [
-                    np.char.add(
-                        np.char.add(c, elements.UNICODE_SQUARES),
-                        elements.COLOR_RESET_CODE,
-                    )
-                    for c in colors
-                ]
-            )
-            index = color[non_zero_mask] % len(colors), new_pix[non_zero_mask]
-        else:
-            decoder_c = np.array(elements.UNICODE_SQUARES)
-            index = new_pix[non_zero_mask]
-        decoder_c[..., 0] = ""
-        char_matrix[non_zero_mask] = decoder_c[index]
+    decoder_c[..., 0] = ""
+    char_matrix[non_zero_mask] = decoder_c[index]
+
+    # if options.force_ascii:
+    #     # If using ASCII characters
+    #     for row in range(options.height):
+    #         for col in range(options.width):
+    #             char_matrix[row, col] = elements.character_for_ascii_pixel(
+    #                 px_matrix[row, col],
+    #                 options.force_ascii_characters,
+    #                 color_mode=options.color,
+    #             )
+    # elif options.character_set == "braille":
+    #     # If using Braille characters
+    #     for row in range(options.height):
+    #         for col in range(options.width):
+    #             char_matrix[row, col] = elements.character_for_2by4_pixels(
+    #                 px_matrix[4 * row : 4 * row + 4, 2 * col : 2 * col + 2],
+    #                 color_mode=options.color,
+    #             )
+    # else:
+    #     # If using Box characters
+    #     color = None
+    #     # Used to be character_for_2by2_pixels
+    #     # TODO Use such a logic also for the other character sets
+    #     encoder = np.array([1, 2, 4, 8], ndmin=3)
+    #     mat = np.swapaxes(
+    #         px_matrix.reshape(height // 2, 2, width // 2, 2), 1, 2
+    #     ).reshape((height // 2, width // 2, 4))
+    #     if options.color:
+    #         color = mat.max(axis=(2)) - 1  # check color
+    #         mat = np.clip(mat, a_min=0, a_max=1)  # type: ignore
+    #     new_pix = (mat * encoder).sum(axis=(2))  # decoder
+    #     non_zero_mask = new_pix != 0
+
+    #     if options.color:
+    #         assert color is not None
+    #         colors = (
+    #             [COLOR_CODES[c] for c in options.color]
+    #             if isinstance(options.color, list)
+    #             else COLOR_CODES.values()
+    #         )
+    #         decoder_c = np.array(
+    #             [
+    #                 np.char.add(
+    #                     np.char.add(c, elements.UNICODE_SQUARES),
+    #                     elements.COLOR_RESET_CODE,
+    #                 )
+    #                 for c in colors
+    #             ]
+    #         )
+    #         index = color[non_zero_mask] % len(colors), new_pix[non_zero_mask]
+    #     else:
+    #         decoder_c = np.array(elements.UNICODE_SQUARES)
+    #         index = new_pix[non_zero_mask]
+    #     decoder_c[..., 0] = ""
+    #     char_matrix[non_zero_mask] = decoder_c[index]
     return char_matrix
 
 
