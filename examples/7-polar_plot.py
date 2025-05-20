6d1fe5594796ab07cca974668d696b7339b21793
Olav Stetter <olav.stetter@googlemail.com>
2025-05-20 06:16:18 +0200

    Added nicer polar plot example.

--- examples/7-polar_plot.py
+++ examples/7-polar_plot.py
@@ -12,6 +12,7 @@ theta = 2 * np.pi * r
 xs = r * np.cos(theta)
 ys = r * np.sin(theta)
 
+print()
 plot(
     xs=xs,
     ys=ys,
@@ -22,6 +23,16 @@ plot(
     x_max=2.1,
     y_min=-2.1,
     y_max=2.1,
-    height=20,
-    width=40,
+    height=30,
+    width=60,
 )
+
+# Ref.: https://www.matematica.pt/en/useful/list-curves.php
+a = 5.0
+b = 7.0
+c = 3
+t = np.arange(0, 200, 0.01)
+xs = (a - b) * np.cos(t) + c * np.cos((a / b - 1) * t)
+ys = (a - b) * np.sin(t) + c * np.sin((a / b - 1) * t)
+print()
+plot(xs=xs, ys=ys, lines=True, color=True, title="A hypotrochoid", height=30, width=60)
